c940675b8f31b5228eab03cf70e89b942bb88a7c
Arunsamuvel <arun15ee03@gmail.com>
2024-07-24 11:34:34 +0530

    error message section added

--- script/script.js
+++ script/script.js
@@ -19,30 +19,39 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKeys}`);
-  //   here the response from the api url inputs will converted into json file and stored into the data
-  var data = await response.json();
-  console.log(data);
-  // here after the converting the json file we use object dot notation method to get the right inputs from the json
-  document.querySelector(".city").innerHTML = data.name;
-  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-  document.querySelector(".humd").innerHTML = data.main.humidity + "%";
-  document.querySelector(".wind").innerHTML = data.wind.speed + "Km/h";
 
-  // if else statement for creating weather icons
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    //   here the response from the api url inputs will converted into json file and stored into the data
+    var data = await response.json();
+    console.log(data);
+    // here after the converting the json file we use object dot notation method to get the right inputs from the json
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°C";
+    document.querySelector(".humd").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "Km/h";
 
-  if (data.weather[0].main == "Clouds") {
-    weatherIcon.src = "Assets/images/clouds.png";
-  } else if (data.weather[0].main == "Clear") {
-    weatherIcon.src = "Assets/images/clear.png";
-  } else if (data.weather[0].main == "Rain") {
-    weatherIcon.src = "Assets/images/rain.png";
-  } else if (data.weather[0].main == "Drizzle") {
-    weatherIcon.src = "Assets/images/drizzle.png";
-  } else if (data.weather[0].main == "Mist") {
-    weatherIcon.src = "Assets/images/mist.png";
+    // if else statement for creating weather icons
+
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "Assets/images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "Assets/images/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "Assets/images/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "Assets/images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "Assets/images/mist.png";
+    }
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
   }
+  // here i call the function when the button is clicked
 }
-// here i call the function when the button is clicked
 searchBtn.addEventListener("click", () => {
   checkWeather(searchBox.value);
 });
